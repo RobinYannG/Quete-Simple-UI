649501bd322f0173d16053058e71780fd4ba1ba3
RobinYannG <robin.goudy@gmail.com>
2017-02-23 23:42:34 +0100

    Quete Simple

---
+++ app/src/main/java/fr/wcs/quetesimpleui/MainActivity.java
@@ -0,0 +1,33 @@
+package fr.wcs.quetesimpleui;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import static fr.wcs.quetesimpleui.R.id.checkBox;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button) ;
+        button.setOnClickListener(this) ;
+    }
+
+
+    public void onClick(View v) {
+        if (v == button) {
+            Toast.makeText(MainActivity.this, "Félicitations ",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
