43602dcf0be024d7cfb6a409d1ec133cf92e642c
RobinYannG <robin.goudy@gmail.com>
2017-02-24 17:51:19 +0100

    QueteSempleUIV2

--- app/src/main/java/fr/wcs/quetesimpleui/MainActivity.java
+++ app/src/main/java/fr/wcs/quetesimpleui/MainActivity.java
@@ -10,10 +10,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
 public class MainActivity extends Activity {
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,39 +23,38 @@ public class MainActivity extends Activity {
         final Button button = (Button) findViewById(R.id.button);
         final TextView textView2 = (TextView) findViewById(R.id.textView2);
 
-
         editText.setEnabled(false);
         editText3.setEnabled(false);
         button.setEnabled(false);
         textView2.setVisibility(View.INVISIBLE);
-
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 editText.setEnabled(checkBox.isChecked());
                 editText3.setEnabled(checkBox.isChecked());
                 button.setEnabled(checkBox.isChecked());
-            }
 
+                if (!isChecked){
+                    textView2.setVisibility(View.INVISIBLE);
+                    editText.setText(null);
+                    editText3.setText(null);
+                }
+            }
         });
 
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 if (editText.length() == 0 || editText3.length() ==0){
-                    Toast.makeText(MainActivity.this, "Tu dois renseigner le nom et le prénom ! ",
+                    Toast.makeText(MainActivity.this, getString(R.string.Toast),
                             Toast.LENGTH_LONG).show();
                     textView2.setVisibility(View.INVISIBLE);
                 }
                 else {
                     textView2.setVisibility(View.VISIBLE);
-                    textView2.setText("Félicitations " + editText.getText() + " " + editText3.getText());
+                    textView2.setText(getString(R.string.Felicitation) + editText.getText() + " " + editText3.getText());
                 }
-
-
             }
         });
-
-
     }
 
 }
